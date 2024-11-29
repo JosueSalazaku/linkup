6f408e580d8fbde8451dd35783bb17367a3bba06
Josué Salazaku <josuesalazaku@gmail.com>
2024-11-29 11:53:53 +0100

    Refactor Nav component for improved layout and styling

--- src/app/components/Nav.tsx
+++ src/app/components/Nav.tsx
@@ -1,17 +1,19 @@
 import Link from "next/link";
 
 function Nav() {
-  return (
-    <nav className="w-screen flex flex-row justify-start items-center">
-      <ul className="w-screen flex flex-row justify-stretch items-center gap-4 py-4  px-4">
-        <li><Link href="/">Link Up</Link></li>
-        <li><Link href="/">Logo</Link></li>
-        <li><Link href="/events">Events</Link></li>
-        <li><Link href="/findus">Find us</Link></li>
-        <li><Link href="/shop">Shop</Link></li>
-      </ul>
-    </nav>
-  );
+    return (
+        <nav className="w-screen h-24 flex justify-between items-center">
+            <ul className="w-full flex justify-between items-center text-xl gap-4 py-4 px-10">
+                <li><Link href="/">LINK UP</Link></li>
+                <li className="absolute left-1/2 transform -translate-x-1/2 text-3xl border-2 px-8 py-4 rounded-xl"><Link href="/">Logo</Link></li>
+                <div className="flex gap-4">
+                    <li><Link href="/events">Events</Link></li>
+                    <li><Link href="/findus">Find us</Link></li>
+                    <li><Link href="/shop">Shop</Link></li>
+                </div>
+            </ul>
+        </nav>
+    );
 }
 
 export default Nav;
