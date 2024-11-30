0165b0c3d997c2402cc7fbf3836fe0df50300895
Josué Salazaku <josuesalazaku@gmail.com>
2024-11-30 16:42:19 +0100

    Enhance footer styling, update Tailwind configuration with new color palette, and add global CSS styles

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -3,31 +3,32 @@ import type { Config } from "tailwindcss";
 export default {
     darkMode: ["class"],
     content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
-  ],
-  theme: {
-  	screens: {
-  		sm: '640px',
-  		md: '768px',
-  		lg: '1024px',
-  		xl: '1400px'
-  	},
-  	extend: {
-  		colors: {
-  			background: 'var(--background)',
-  			foreground: 'var(--foreground)'
-  		},
-  		fontFamily: {
-  			sans: ['Poppins']
-  		},
-  		borderRadius: {
-  			lg: 'var(--radius)',
-  			md: 'calc(var(--radius) - 2px)',
-  			sm: 'calc(var(--radius) - 4px)'
-  		}
-  	}
-  },
-  plugins: [require("tailwindcss-animate")],
+        "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+        "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+        "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    ],
+    theme: {
+        screens: {
+            sm: '640px',
+            md: '768px',
+            lg: '1024px',
+            xl: '1400px'
+        },
+        extend: {
+            colors: {
+                primaryDark: '#364866', // Dark blue
+                secondaryDark: '#4c5c76', // Blue-gray
+                accentYellow: '#dda93d', // Golden yellow
+                accentOrange: '#ca641f', // Burnt orange
+            },
+            fontFamily: {
+                sans: ['Poppins']
+            },
+            borderRadius: {
+                lg: 'var(--radius)',
+                md: 'calc(var(--radius) - 2px)',
+                sm: 'calc(var(--radius) - 4px)'
+            }
+        }
+    },
 } satisfies Config;
